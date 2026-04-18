ec2ce50a62ed9158beecc8d4339ffbd643b21b85
AshlyAquino <aquinoashly2807@gmail.com>
2026-04-18 16:01:15 -0600

    Primer commit

---
+++ src/main/java/com/mycompany/laboratorio2_aaquino/Laboratorio2_aaquino.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.laboratorio2_aaquino;
+
+/**
+ *
+ * @author aquin
+ */
+public class Laboratorio2_aaquino {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
